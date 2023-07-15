70bd9c121afe954488558ad322846e17b2a706d3
aebrahim10 <aebrahim10@gmail.com>
2023-07-15 11:45:41 -0400

    Inluded sorting, searching in the table

--- src/components/EmpPages/AllEmployees.js
+++ src/components/EmpPages/AllEmployees.js
@@ -4,13 +4,19 @@ import Controls from "../EmpComponents/controls/Controls";
 //import { Search } from "@material-ui/icons";
 import Search from '@mui/icons-material/Search';
 import PeopleOutlineIcon from '@mui/icons-material/PeopleOutline';
+import PeopleOutlineTwoToneIcon from '@material-ui/icons/PeopleOutlineTwoTone';
 import PageHeader from '../EmpComponents/PageHeader';
 //import AddIcon from '@material-ui/icons/Add';
 import AddIcon from '@mui/icons-material/AddCircleOutline';
+// import AddIcon from '@material-ui/icons/Add';
 import EmployeeForm from './EmployeeForm';
 import Popup from '../EmpComponents/Popup';
 import useTable from '../EmpComponents/useTable';
 import * as employeeService from '../EmpServices/employeeService'
+import EditOutlinedIcon from '@material-ui/icons/EditOutlined';
+import CloseIcon from '@material-ui/icons/Close';
+import Notification from "../EmpComponents/Notification";
+import ConfirmDialog from "../EmpComponents/ConfirmDialog";
 
 const useStyles = makeStyles(theme => ({
     pageContent: {
@@ -27,91 +33,159 @@ const useStyles = makeStyles(theme => ({
 }))
 
 
-export default function AllEmployees() {
-  const [records, setRecords] = useState(employeeService.getAllEmployees())
-  const [recordForEdit, setRecordForEdit] = useState(null)
-  const [openPopup, setOpenPopup] = useState(false)
-  const [filterFn, setFilterFn] = useState({ fn: items => { return items; } })
+const headCells = [
+    { id: 'fullName', label: 'Employee Name' },
+    { id: 'email', label: 'Email Address (Personal)' },
+    { id: 'mobile', label: 'Mobile Number' },
+    { id: 'department', label: 'Department' },
+    { id: 'actions', label: 'Actions', disableSorting: true }
+]
 
-  const headCells = [
-    {id:'fullName', label:'Full Name'},
-    {id:'email', label:'Email address'},
-    {id:'mobile', label:'Mobile number'},
-    {id:'deparment', label:'Department'},
-  ]
+export default function Employees() {
 
-  const handleSearch = e => {
-    let target = e.target;
-    setFilterFn({
-        fn: items => {
-            if (target.value == "")
-                return items;
-            else
-                return items.filter(x => x.fullName.toLowerCase().includes(target.value))
-        }
-    })
-}
-  const {
-            TblContainer, TblHead, TblPagination, recordsAfterPagingAndSorting
-         } = useTable(records, headCells)
-  const classes = useStyles();
-  
-  return (
-  <>
-    <PageHeader
-    title="New Employee"
-    subTitle="Form design with validation"
-    icon={<PeopleOutlineIcon fontSize="large" />}  />
+    const classes = useStyles();
+    const [recordForEdit, setRecordForEdit] = useState(null)
+    const [records, setRecords] = useState(employeeService.getAllEmployees())
+    const [filterFn, setFilterFn] = useState({ fn: items => { return items; } })
+    const [openPopup, setOpenPopup] = useState(false)
+    const [notify, setNotify] = useState({ isOpen: false, message: '', type: '' })
+    const [confirmDialog, setConfirmDialog] = useState({ isOpen: false, title: '', subTitle: '' })
 
-    {/* <Paper className={classes.pageContent}>
-        <EmployeeForm />
-    </Paper> */}
+    const {
+        TblContainer,
+        TblHead,
+        TblPagination,
+        recordsAfterPagingAndSorting
+    } = useTable(records, headCells, filterFn);
 
+    const handleSearch = e => {
+        let target = e.target;
+        setFilterFn({
+            fn: items => {
+                if (target.value == "")
+                    return items;
+                else
+                    return items.filter(x => x.fullName.toLowerCase().includes(target.value))
+            }
+        })
+    }
 
-   <Paper className={classes.pageContent}>
-    <Toolbar>
-        <Controls.Input
-            label="Search Employees"
-            className={classes.searchInput}
-            InputProps={{
-                startAdornment: (<InputAdornment position="start">
-                    <Search />
-                </InputAdornment>)
-            }}
-            onChange={handleSearch}
-        />
-        <Controls.Button
-            text="Add New"
-            variant="outlined"
-            startIcon={<AddIcon />}
-            className={classes.newButton}
-            onClick={() => { setOpenPopup(true); setRecordForEdit(null); }}
-        />
-    </Toolbar>
-     <TblContainer>
-        <TblHead />
-        <TableBody>{
-            recordsAfterPagingAndSorting().map(record =>(<TableRow> 
-                                    <TableCell>{record.fullName}</TableCell>
-                                    <TableCell>{record.email}</TableCell>
-                                    <TableCell>{record.mobile}</TableCell>
-                                    <TableCell>{record.department}</TableCell>
-                                  </TableRow> ))}
-            
+    const addOrEdit = (employee, resetForm) => {
+        if (employee.id == 0)
+            employeeService.insertEmployee(employee)
+        else
+            employeeService.updateEmployee(employee)
+        resetForm()
+        setRecordForEdit(null)
+        setOpenPopup(false)
+        setRecords(employeeService.getAllEmployees())
+        setNotify({
+            isOpen: true,
+            message: 'Submitted Successfully',
+            type: 'success'
+        })
+    }
 
-        </TableBody>
-     </TblContainer>
-     <TblPagination />
-    </Paper> 
-     <Popup title='Employee Form' openPopup={openPopup} setOpenPopup={setOpenPopup}>
-       <EmployeeForm />
+    const openInPopup = item => {
+        setRecordForEdit(item)
+        setOpenPopup(true)
+    }
 
-     </Popup>
+    const onDelete = id => {
+        setConfirmDialog({
+            ...confirmDialog,
+            isOpen: false
+        })
+        employeeService.deleteEmployee(id);
+        setRecords(employeeService.getAllEmployees())
+        setNotify({
+            isOpen: true,
+            message: 'Deleted Successfully',
+            type: 'error'
+        })
+    }
 
+    return (
+        <>
+            <PageHeader
+                title="New Employee"
+                subTitle="Form design with validation"
+                icon={<PeopleOutlineTwoToneIcon fontSize="large" />}
+            />
+            <Paper className={classes.pageContent}>
 
-
-  </>
-    
-
-  )
+                <Toolbar>
+                    <Controls.Input
+                        label="Search Employees"
+                        className={classes.searchInput}
+                        InputProps={{
+                            startAdornment: (<InputAdornment position="start">
+                                <Search />
+                            </InputAdornment>)
+                        }}
+                        onChange={handleSearch}
+                    />
+                    <Controls.Button
+                        text="Add New"
+                        variant="outlined"
+                        startIcon={<AddIcon />}
+                        className={classes.newButton}
+                        onClick={() => { setOpenPopup(true); setRecordForEdit(null); }}
+                    />
+                </Toolbar>
+                <TblContainer>
+                    <TblHead />
+                    <TableBody>
+                        {
+                            recordsAfterPagingAndSorting().map(item =>
+                                (<TableRow key={item.id}>
+                                    <TableCell>{item.fullName}</TableCell>
+                                    <TableCell>{item.email}</TableCell>
+                                    <TableCell>{item.mobile}</TableCell>
+                                    <TableCell>{item.department}</TableCell>
+                                    <TableCell>
+                                        <Controls.ActionButton
+                                            color="primary"
+                                            onClick={() => { openInPopup(item) }}>
+                                            <EditOutlinedIcon fontSize="small" />
+                                        </Controls.ActionButton>
+                                        <Controls.ActionButton
+                                            color="secondary"
+                                            onClick={() => {
+                                                setConfirmDialog({
+                                                    isOpen: true,
+                                                    title: 'Are you sure to delete this record?',
+                                                    subTitle: "You can't undo this operation",
+                                                    onConfirm: () => { onDelete(item.id) }
+                                                })
+                                            }}>
+                                            <CloseIcon fontSize="small" />
+                                        </Controls.ActionButton>
+                                    </TableCell>
+                                </TableRow>)
+                            )
+                        }
+                    </TableBody>
+                </TblContainer>
+                <TblPagination />
+            </Paper>
+            <Popup
+                title="Employee Form"
+                openPopup={openPopup}
+                setOpenPopup={setOpenPopup}
+            >
+                <EmployeeForm
+                    recordForEdit={recordForEdit}
+                    addOrEdit={addOrEdit} />
+            </Popup>
+            <Notification
+                notify={notify}
+                setNotify={setNotify}
+            />
+            <ConfirmDialog
+                confirmDialog={confirmDialog}
+                setConfirmDialog={setConfirmDialog}
+            />
+        </>
+    )
 }
